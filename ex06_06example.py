eb3235fa48b9e0860a4c4c1ffb85d7c728802d9c
pwkandt <123347213+pwkandt@users.noreply.github.com>
2023-04-10 23:21:36 -0400

    Create ex06_06example.py

---
+++ ex06_06example.py
@@ -0,0 +1,5 @@
+string1 = 'What do you do when you can not do anything, but there is nothing you can do? You do what you can.'
+string2 = 'you'
+
+total = string1.count(string2[0:50]) 
+print(total) 
